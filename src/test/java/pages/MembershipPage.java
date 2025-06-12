af639c301dc49d35aa523606d13cc27aa42e2404
Kevin <kharris891@gmail.com>
2025-06-12 15:03:38 -0400

    Revert "UC 6 first commit"

--- src/test/java/pages/MembershipPage.java
+++ src/test/java/pages/MembershipPage.java
@@ -1,13 +1,4 @@
 package pages;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import utils.CommonMethods;
-
-public class MembershipPage extends CommonMethods {
-
-    @FindBy(id="")
-    public WebElement;
-
 public class MembershipPage {
 }

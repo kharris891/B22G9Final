7c27673f9c39a4e9024f53792f2654c026521516
kharris891 <kharris891@gmail.com>
2025-06-11 20:58:27 -0400

    UC 6 first commit

---
+++ src/test/java/pages/MembershipPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utils.CommonMethods;
+
+public class MembershipPage extends CommonMethods {
+
+    @FindBy(id="")
+    public WebElement;
+}
